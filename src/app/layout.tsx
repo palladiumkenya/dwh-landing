51e2be028bff7176752417f975f79e7cf0a20b17
koskedk <koskedk@live.com>
2025-05-09 18:08:08 +0300

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const ibmSans = IBM_Plex_Sans({
     variable: "--font-geist-sans", // You can keep this variable name for compatibility
